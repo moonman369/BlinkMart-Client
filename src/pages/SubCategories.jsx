31ffaceac2c07a5858c8c1778302ad6da967d0d4
Ayan Maiti <mightyayan369@gmail.com>
2025-02-09 12:38:32 +0530

    Add AddSubcategoryModal component; implement subcategory creation functionality and integrate with SubCategories page

--- src/pages/SubCategories.jsx
+++ src/pages/SubCategories.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import AddSubcategoryModal from "../components/AddSubcategoryModal";
 
 const SubCategories = () => {
-  return <div>SubCategories</div>;
+  const [openAddSubcategoryModal, setOpenAddSubcategoryModal] = useState(false);
+
+  return (
+    <section>
+      <div className="p-3 font-semibold bg-gray-900 shadow-secondary-200 shadow-md rounded-md flex items-center justify-between">
+        <h2 className="text-[20px]">Subcategories</h2>
+        <button
+          className={`text-white p-3 rounded font-semibold tracking-wider bg-green-700 hover:bg-green-800`}
+          onClick={() => setOpenAddSubcategoryModal(true)}
+        >
+          Add Subcategory
+        </button>
+      </div>
+
+      {openAddSubcategoryModal && (
+        <AddSubcategoryModal
+          closeModal={() => setOpenAddSubcategoryModal(false)}
+        />
+      )}
+    </section>
+  );
 };
 
 export default SubCategories;
